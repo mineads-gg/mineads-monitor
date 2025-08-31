b2b09800eb960db31a697b0ddb6e68ea63e189e2
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-08-31 06:58:37 +0200

    feat: add more licensing stuff

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,23 +1,28 @@
 plugins {
-    `kotlin-dsl`
+  `kotlin-dsl`
 }
 
 repositories {
-    gradlePluginPortal()
-    mavenCentral()
+  gradlePluginPortal()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("com.gradleup.shadow:shadow-gradle-plugin:9.1.0")
-    implementation("xyz.wagyourtail.jvmdowngrader:xyz.wagyourtail.jvmdowngrader.gradle.plugin:1.3.3")
+  implementation("com.gradleup.shadow:shadow-gradle-plugin:9.1.0")
+  implementation("com.diffplug.spotless:spotless-plugin-gradle:7.2.1")
+  implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:6.2.5")
+  implementation("net.kyori:indra-git:3.2.0")
+  implementation("xyz.wagyourtail.jvmdowngrader:xyz.wagyourtail.jvmdowngrader.gradle.plugin:1.3.3")
+  implementation("net.ltgt.errorprone:net.ltgt.errorprone.gradle.plugin:4.3.0")
+  implementation("io.freefair.gradle:lombok-plugin:8.14.2")
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
+  }
 }
 
 kotlin {
-    jvmToolchain(17)
+  jvmToolchain(17)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,5 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-    }
-    plugins {
-        id("com.gradleup.shadow") version "9.1.0"
-        id("org.cadixdev.licenser") version "0.6.1"
-        id("net.kyori.indra") version "3.2.0"
-        id("net.kyori.indra.git") version "3.2.0"
-        id("net.kyori.indra.publishing") version "3.2.0"
-        id("net.kyori.blossom") version "2.1.0"
-    }
-}
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
