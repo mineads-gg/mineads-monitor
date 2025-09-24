df08492f6978e7fb65b1abb10381bc2f8d5a19ee
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-09-24 10:08:44 +0200

    fix: prevent publishing test fixtures on maven

--- buildSrc/src/main/kotlin/mm.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/mm.java-conventions.gradle.kts
@@ -79,6 +79,10 @@ java {
   }
 }
 
+val javaComponent = components["java"] as AdhocComponentWithVariants
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
+
 publishing {
   repositories {
     maven("https://maven.pkg.github.com/mineads-gg/mineads-monitor") {
